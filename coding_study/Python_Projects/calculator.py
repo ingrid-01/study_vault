10ed0b1eafddcf4184de8cbf7587c33ed7b475f6
Ingrid <lkoh93@gmail.com>
2026-01-11 16:19:22 +0900

    vault backup: 2026-01-11 16:19:21

---
+++ coding_study/Python_Projects/calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("첫 번째 숫자를 입력하세요: "))
+op = input("연산자를 입력하세요 (+, -, *, /): ")
+num2 = float(input("두 번째 숫자를 입력하세요: "))
+
+def calculate(a, b, op):
+  if op == '+':
+    return a + b
+  elif op == '-':
+    return a - b
+  elif op == '*':
+    return a * b
+  elif op == '/':
+    if b != 0:
+      return "0으로 나눌 수 없습니다."
+    return a / b
+  else:
+      return "잘못된 연산자입니다."
+
+result = calculate(num1, num2, op)
+print(f"결과:", result)
