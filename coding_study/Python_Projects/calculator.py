2dee44abc8aee37b492a2d64197546cda24db2b5
Ingrid <lkoh93@gmail.com>
2026-01-11 16:24:23 +0900

    vault backup: 2026-01-11 16:24:23

--- coding_study/Python_Projects/calculator.py
+++ coding_study/Python_Projects/calculator.py
@@ -1,6 +1,6 @@
-num1 = float(input("첫 번째 숫자를 입력하세요: "))
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
 op = input("연산자를 입력하세요 (+, -, *, /): ")
-num2 = float(input("두 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
 
 def calculate(a, b, op):
   if op == '+':
