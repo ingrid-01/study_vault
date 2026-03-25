2ee662b08aba6c053905bf7b03785c3b62116354
Ingrid <lkoh93@gmail.com>
2026-03-25 20:19:56 +0900

    add 카페 추가하기 버트 via JS

--- mar_study/prac10-js-basics/script.js
+++ mar_study/prac10-js-basics/script.js
@@ -1,1 +1,12 @@
 const btn = document.getElementById('btn');
+const cafeList = document.getElementById('cafe-list');
+
+const cafes = ["포일 청년발전소", "스타벅스", "오픈커피"];
+
+btn.addEventListener('click', () => {
+  cafes.forEach((cafe) => {
+    const li = document.createElement('li');
+    li.textContent = cafe;
+    cafeList.appendChild(li);
+  });
+});
