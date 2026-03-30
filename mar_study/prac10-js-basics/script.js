92de08f8f7c3885fad07f3ddca6fd645f8b2aedd
Ingrid <lkoh93@gmail.com>
2026-03-30 18:32:58 +0900

    add ternary operator for parking

--- mar_study/prac10-js-basics/script.js
+++ mar_study/prac10-js-basics/script.js
@@ -1,9 +1,13 @@
 const cafeList = document.getElementById("cafe-list");
 
-const cafes = ["포일 청년발전소", "스타벅스", "오픈커피"];
+const cafes = [
+  { name: "포일 청년발전소", region: "의왕", parking: true },
+  { name: "스타벅스", region: "의왕", parking: false },
+  { name: "오픈커피", region: "의왕", parking: true },
+];
 
 cafes.forEach((cafe) => {
   const li = document.createElement("li");
-  li.textContent = cafe;
+  li.textContent = `${cafe.name} - ${cafe.region} - 주차: ${cafe.parking ? `✅` : `❌`}`;
   cafeList.appendChild(li);
 });
