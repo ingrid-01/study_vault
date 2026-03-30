97bf65fdbf639c4980249e083a5c7f69854370ce
Ingrid <lkoh93@gmail.com>
2026-03-30 18:12:02 +0900

    delete button

--- mar_study/prac10-js-basics/script.js
+++ mar_study/prac10-js-basics/script.js
@@ -1,14 +1,9 @@
-const btn = document.getElementById("btn");
 const cafeList = document.getElementById("cafe-list");
 
 const cafes = ["포일 청년발전소", "스타벅스", "오픈커피"];
 
-btn.addEventListener("click", () => {
-  cafeList.innerHTML = ""; // 기존 리스트 초기화
-
-  cafes.forEach((cafe) => {
-    const li = document.createElement("li");
-    li.textContent = cafe;
-    cafeList.appendChild(li);
-  });
+cafes.forEach((cafe) => {
+  const li = document.createElement("li");
+  li.textContent = cafe;
+  cafeList.appendChild(li);
 });
