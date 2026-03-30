ae8110997c4edaed1b85c3cdbc2ef5e87a1f9fca
Ingrid <lkoh93@gmail.com>
2026-03-30 17:39:47 +0900

    add innerhtml method

--- mar_study/prac10-js-basics/script.js
+++ mar_study/prac10-js-basics/script.js
@@ -1,11 +1,13 @@
-const btn = document.getElementById('btn');
-const cafeList = document.getElementById('cafe-list');
+const btn = document.getElementById("btn");
+const cafeList = document.getElementById("cafe-list");
 
 const cafes = ["포일 청년발전소", "스타벅스", "오픈커피"];
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
+  cafeList.innerHTML = ""; // 기존 리스트 초기화
+
   cafes.forEach((cafe) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.textContent = cafe;
     cafeList.appendChild(li);
   });
